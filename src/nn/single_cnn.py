c5f40eb805e58712c60194ed3cbf79a18079a50b
bohyun-shin <bohyun.shin@linecorp.com>
2024-07-23 08:22:16 +0900

    Add max pooling

---
+++ src/nn/single_cnn.py
@@ -0,0 +1,6 @@
+from nn.base import BaseNeuralNet
+
+
+class SingleCNN(BaseNeuralNet):
+    def __init__(self):
+        super().__init__()
--- src/tools/activations.py
+++ src/tools/activations.py
@@ -7,3 +7,18 @@ class Sigmoid:
 
     def forward(self, x):
         return 1 / (1 + np.exp(-x))
+
+
+class MaxPooling:
+    def __init__(self, k: int):
+        self.k = k
+
+    def forward(self, x):
+        h,w = x.shape
+        out_h = h-self.k+1
+        out_w = w-self.k+1
+        res = []
+        for i in range(out_h):
+            for j in range(out_w):
+                res.append( x[i:i+self.k , j:j+self.k].max() )
+        return np.array(res).reshape((out_h, out_w))
