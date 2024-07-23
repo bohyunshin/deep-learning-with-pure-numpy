86c9f59f9a706f9dc84dad16047a5cd109ab4cf7
bohyun-shin <bohyun.shin@linecorp.com>
2024-07-24 08:04:50 +0900

    Add single layer cnn neural network

--- src/nn/single_cnn.py
+++ src/nn/single_cnn.py
@@ -1,6 +1,28 @@
+import numpy as np
+
 from nn.base import BaseNeuralNet
+from nn.modules import Convolution
+from tools.activations import MaxPooling, Sigmoid
 
 
 class SingleCNN(BaseNeuralNet):
-    def __init__(self):
+    def __init__(self, input_dim: tuple, k: int , zero_padding: int, pooling_k: int):
         super().__init__()
+        self.cnn = Convolution(
+            input_dim=input_dim,
+            k=k,
+            zero_padding=zero_padding
+        )
+        self.max_pooling = MaxPooling(k=pooling_k)
+        self.sigmoid = Sigmoid()
+
+    def forward(self, x):
+        x = self.cnn.forward(x)
+        x = self.max_pooling.forward(x)
+        x = self.sigmoid.forward(x)
+        return x
+
+if __name__ == "__main__":
+    cnn = SingleCNN((4,4), 3, 1, 2)
+    x = np.random.normal(1,0.5,(4,4))
+    print(cnn.forward(x))
