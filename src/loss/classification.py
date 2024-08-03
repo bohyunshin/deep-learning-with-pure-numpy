ab3947e29955dbcef140639f6a955ad11a66a53f
bohyun-shin <bohyun.shin@linecorp.com>
2024-08-03 10:51:23 +0900

    Modify cross entropy function wrt a batch

--- src/loss/classification.py
+++ src/loss/classification.py
@@ -2,6 +2,16 @@ import numpy as np
 
 
 def cross_entropy(y_true, y_pred):
+    """
+    params
+    ------
+    y_true: np.ndarray (n, n_label)
+    y_pred: np.ndarray (n, n_label)
+
+    returns
+    -------
+    grad: np.ndarray (n,)
+    """
     y_true = y_true.reshape(-1)
     y_pred = y_pred.reshape(-1)
-    return -(y_true * np.log2(y_pred)).sum()
+    return -(y_true * np.log2(y_pred)).sum(axis=1) # (n,)
