Fix backprop of softmax

src/nn/mlp.py
@@ -1,13 +1,63 @@
 import numpy as np
 
 from nn.base import BaseNeuralNet
-from tools.activations import Relu
+from tools.activations import Relu, Softmax
 from nn.modules import Linear
-
-np.random.seed(1)
 
 
 class MultipleLayerPerceptronRegression:
+    def __init__(self, struct, n, model="regression"):
+        super().__init__()
+        self.struct = struct
+        self.n = n
+        if model not in ["regression", "classification"]:
+            raise
+        self.model = model
+        self.layers = []
+        self.gradient_step_layers = []
+        for i in range(1, len(struct)):
+            fc = Linear(struct[i-1], struct[i])
+            self.layers.append(fc)
+            self.gradient_step_layers.append(fc)
+            self.layers.append(Relu())
+        if self.model == "classification":
+            self.layers.append(Softmax())
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, y, pred):
+        """
+        params
+        ------
+        y: np.ndarray ( reg: (n,1), classification: (n,L) )
+            True label.
+
+        pred: np.ndarray ( reg: (n,1), classification: (n,L) )
+            Prediction value.
+
+        """
+        # calculate initial gradient w.r.t. loss function
+        if self.model == "regression":
+            dx = (pred - y) / self.n * 2
+        else:
+            dx = -y / pred + (1-y) / (1-pred)
+
+        # backpropagation
+        for layer in self.layers[::-1]:
+            dx = layer.backward(dx)
+
+    def step(self, lr):
+        for layer in self.gradient_step_layers:
+            params_info = layer.get_params_grad()
+            for param,info in params_info.items():
+                param_grad_step = info["current"] - lr*info["grad"]
+                setattr(layer, param, param_grad_step)
+
+
+class MultipleLayerPerceptronClassification:
     def __init__(self, struct, n):
         super().__init__()
         self.struct = struct
@@ -19,23 +69,3 @@ class MultipleLayerPerceptronRegression:
             self.layers.append(fc)
             self.gradient_step_layers.append(fc)
             self.layers.append(Relu())
-
-    def forward(self, x):
-        self.activated_val = [x]
-        for layer in self.layers:
-            x = layer.forward(x)
-            if layer.__class__.__name__ == "Sigmoid":
-                self.activated_val.append(x)
-        return x
-
-    def backward(self, y, pred):
-        dx = (pred - y) / self.n * 2
-        for layer in self.layers[::-1]:
-            dx = layer.backward(dx)
-
-    def step(self, lr):
-        for layer in self.gradient_step_layers:
-            params_info = layer.get_params_grad()
-            for param,info in params_info.items():
-                param_grad_step = info["current"] - lr*info["grad"]
-                setattr(layer, param, param_grad_step)

src/nn/modules.py
@@ -20,7 +20,7 @@ class Linear:
         x = np.dot(x, self.w) + self.b.reshape(1, self.output_dim)
         return x # shape: (num_of_data, output_dim)
 
-    def backward(self, dx_out):
+    def backward(self, dx_out, **kwargs):
         """
         params
         ------

src/tools/activations.py
@@ -105,16 +105,47 @@ class Softmax:
         x = np.exp(x - x.max(axis=1).reshape(-1,1))
         y_pred = x / x.sum(axis=1).reshape(-1,1)
         self.y_pred = y_pred
+        self.x = x
         return y_pred
 
-    def backward(self, y_true):
+    def backward(self, dx_out):
         """
+        Step 1
+        Calculate jacobian matrix (L, L)
+        dyhat_i1/dz_i1 ... dyhat_i1/dz_iL
+        ...
+        dyhat_iL/dz_i1 ... dyhat_LL/dz_iL
+
+        Note that for each data point, L x L jacobian matrix should be calculated. (N, L, L)
+
+        Step 2
+        Calculate downstream gradient
+        dL/dz_ik = \sum_j dyhat_ij/dz_ik x dL/dhat_ij
+
+        Note that because it is downstream gradient, its dimension is (N, L)
+
         params
         ------
-        y_true: np.ndarray (n, n_label)
+        dx_out: np.ndarray (n, n_label)
 
         returns
         -------
         out_grad: np.ndarray (n, n_label)
         """
-        return self.y_pred - y_true
+        # step 1
+        n, L = dx_out.shape
+        jacobian = np.zeros((n, L, L))
+        I = np.identity(L)
+        for i in range(n):
+            y_pred_i = self.y_pred[i]
+            right = I - np.tile(y_pred_i, L).reshape(L, L)
+            left = np.tile(y_pred_i, L).reshape(L, L).T
+            jacobian[i] = left * right
+
+        # step 2
+        dx_in = np.zeros((n, L))
+        for i in range(n):
+            dL_dz_ij = np.dot(dx_out[i], jacobian[i]) # (1, L)
+            dx_in[i] = dL_dz_ij
+
+        return dx_in

test/test_activations.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.getcwd(), "src"))
 import numpy as np
-from tools.activations import MaxPooling, Relu
+from tools.activations import MaxPooling, Relu, Softmax
 
 
 def test_relu():
@@ -81,3 +81,31 @@ def test_max_pooling_unequal_size():
     in_grad_expected[0][3][2] = out_grad[0][1][0]
     in_grad_expected[0][3][3] = out_grad[0][1][1]
     np.testing.assert_array_equal(in_grad_expected, in_grad)
+
+
+def test_softmax():
+    n = 4
+    L = 3
+
+    logits = np.random.normal(0, 0.5, (n, L))
+    dx_out = np.random.normal(0, 0.5, (n, L))
+
+    softmax = Softmax()
+
+    # forward
+    out = softmax.forward(logits)
+
+    # backward
+    dx_in = softmax.backward(dx_out)
+
+    I = np.identity(L)
+    for i in range(n):
+        yhat = out[i]
+        mat = np.array([
+            yhat, yhat, yhat
+        ])
+        right = I - mat
+        left = mat.T
+        jacobian = left * right
+        grad = np.dot(dx_out[i], jacobian)
+        np.testing.assert_array_almost_equal(dx_in[i], grad)